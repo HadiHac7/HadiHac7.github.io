f432b49d62b3d98647425993f611cf3838f51ec1
HadiHac7 <hadi.hachem@jpschools.org>
2024-06-07 16:32:08 +0000

    added platforms and collectables to game

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -46,7 +46,7 @@ $(function () {
  
     createPlatform(200, 625, 100, 15);
     createPlatform(1000, 625, 200, 15);
-    createPlatform(550, 400, 300, 15);
+    createPlatform(490, 400, 370, 15);
     createPlatform(350, 525, 140, 15);
     createPlatform(850, 525, 100, 15);
     createPlatform(0, 275, 140, 15);
@@ -60,7 +60,34 @@ $(function () {
     createPlatform(200, 400, 25, 10);
     createPlatform(325, 300, 25, 10);
     createPlatform(200, 225, 25, 10);
+    createPlatform(490, 0, 10, 400);
+    createPlatform(550, 625, 300, 15);
+    createPlatform(450, 675, 100, 15);
+    createPlatform(450, 625, 10, 50);
+    createPlatform(350, 625, 100, 15);
+    createPlatform(350, 600, 10, 25);
+    createPlatform(625, 575, 10, 50);
+    createPlatform(700, 575, 10, 50);
+    createPlatform(775, 575, 10, 50);
+    createPlatform(850, 400, 10, 125);
+    createPlatform(850, 625, 10, 75);
+    createPlatform(850, 700, 150, 15);
+    createPlatform(1000, 625, 10, 90);
+    createPlatform(1200, 525, 100, 15)
+    createPlatform(950, 395, 10, 145);
+    createPlatform(1350, 450, 50, 10);
+    createPlatform(950, 390, 300, 15);
+    createPlatform(775, 350, 10, 50);
+    createPlatform(700, 300, 10, 100);
+    createPlatform(625, 250, 10, 150);
+    createPlatform(625, 325, 25, 10);
+    createPlatform(575, 250, 50, 15);
+    createPlatform(500, 325, 25, 10);
+    createPlatform(500, 175, 25, 10);
+    createPlatform(575, 125, 300, 10);
     
+
+
     
 
     
@@ -72,8 +99,8 @@ $(function () {
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-    createCollectable("gumgum", 450, 300, 20, 0.5);
-    createCollectable("enma", 750, 100, 10, 0.1);
+    createCollectable("gumgum", 205, 300, 20, 0.5);
+    createCollectable("enma", 50, 300, 1, 1);
     createCollectable("thousandsunny", 1300, 50, 10, 0.1);
 
 
@@ -83,10 +110,16 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-    createCannon("left", 600, 500);
+    createCannon("left", 600, 1000);
     createCannon("bottom", 275, 2000);
     createCannon("top", 300, 3000);
-    createCannon("left", 0, 2500);
+    createCannon("top", 525, 3500);
+    createCannon("top", 725, 1500);
+    createCannon("top", 800, 2250);
+    createCannon("right", 375, 5500);
+    createCannon("bottom", 1320, 2500);
+    createCannon("top", 970, 3500);
+    createCannon("bottom", 100, 2000)
 
 
 
